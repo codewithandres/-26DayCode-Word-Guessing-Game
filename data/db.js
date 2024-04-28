8db03301924e6d50735425185e761f36fb53bcb8
Dize dev <axiozweb2022@gmail.com>
2024-04-28 10:50:47 -0500

    se agreo la data para el juego

--- data/db.js
+++ data/db.js
@@ -1,42 +1,42 @@
 let words = [
     {
         word: "javascript",
-        description: "Un lenguaje de programación muy popular en el desarrollo web."
+        hint: "Un lenguaje de programación muy popular en el desarrollo web."
     },
     {
         word: "python",
-        description: "Un lenguaje de programación versátil conocido por su sintaxis clara."
+        hint: "Un lenguaje de programación versátil conocido por su sintaxis clara."
     },
     {
         word: "java",
-        description: "Un lenguaje de programación orientado a objetos utilizado en aplicaciones empresariales."
+        hint: "Un lenguaje de programación orientado a objetos utilizado en aplicaciones empresariales."
     },
     {
         word: "ruby",
-        description: "Un lenguaje de programación interpretado y orientado a objetos."
+        hint: "Un lenguaje de programación interpretado y orientado a objetos."
     },
     {
         word: "git",
-        description: "Un sistema de control de versiones distribuido."
+        hint: "Un sistema de control de versiones distribuido."
     },
     {
         word: "react",
-        description: "Una biblioteca de JavaScript para construir interfaces de usuario."
+        hint: "Una biblioteca de JavaScript para construir interfaces de usuario."
     },
     {
         word: "angular",
-        description: "Un marco de trabajo de JavaScript para crear aplicaciones web de una sola página."
+        hint: "Un marco de trabajo de JavaScript para crear aplicaciones web de una sola página."
     },
     {
         word: "vue",
-        description: "Un marco de trabajo de JavaScript progresivo para construir interfaces de usuario."
+        hint: "Un marco de trabajo de JavaScript progresivo para construir interfaces de usuario."
     },
     {
         word: "node",
-        description: "Un entorno de ejecución de JavaScript fuera del navegador."
+        hint: "Un entorno de ejecución de JavaScript fuera del navegador."
     },
     {
         word: "docker",
-        description: "Una plataforma para desarrollar, enviar y ejecutar aplicaciones en contenedores."
+        hint: "Una plataforma para desarrollar, enviar y ejecutar aplicaciones en contenedores."
     }
 ];
